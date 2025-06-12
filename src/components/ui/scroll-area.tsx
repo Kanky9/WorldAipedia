96973522bae19c8fea99703a39c7968303f4bd97
Joaco Bello <joacobello1233@gmail.com>
2025-06-12 15:09:45 +0000

    las barras de scroll que te marque con rojo, y otras que hayas puesto, q

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -17,7 +18,7 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
+    {/* <ScrollBar /> Omitted to hide the custom scrollbar */}
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
