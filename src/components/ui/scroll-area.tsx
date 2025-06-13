9572569f742d5a4becf8d3a81f04e8ac58c48572
Santino Bournot <bournotsantino03@gmail.com>
2025-06-13 23:25:02 +0000

    cambia la barra de scroll que aparece cuando hablamos con el chat de lac

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -18,7 +18,7 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    {/* <ScrollBar /> Omitted to hide the custom scrollbar */}
+    <ScrollBar />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
@@ -41,9 +41,10 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-md bg-primary" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
 
 export { ScrollArea, ScrollBar }
+
