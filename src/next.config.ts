a569771c3763d1ccafed5374156813776bd27a19
Santino Bournot <bournotsantino03@gmail.com>
2025-06-25 14:10:33 +0000

    fix lace chat

--- src/next.config.ts
+++
@@ -1,3 +0,0 @@
-// This configuration file is a duplicate and is not used by Next.js.
-// The active configuration is in the root `next.config.ts` file.
-// This file is being cleared to prevent confusion.
